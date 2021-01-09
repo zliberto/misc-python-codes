1c6a69e7af98f1185c8ffda48e145210752dfb2e
Zuzanna Liberto <zuzannaliberto@gmail.com>
2021-01-09 13:56:28 -0600

    add all files

---
+++ Assignment 4 /arithmetic.py
@@ -0,0 +1,19 @@
+'''
+Zuzanna Liberto
+Assignment 4
+
+This module contains two functions:
+- "add" takes two numbers and returns its sum
+- "get_length" takes a string x and returns its length
+
+'''
+
+def add(x,y):
+    sum = x + y
+    return sum
+    
+def get_length(x):
+    length = len(x)
+    return length
+    
+ 
